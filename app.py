875aad128e7f60076750f38c68fc61f8f9dc8f7d
AlexSacMorane <al.sacmorane@gmail.com>
2025-05-22 16:33:48 +0200

    Add 404 and 500 webpages

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import pandas as pd
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(e):
+ return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+ return render_template('500.html'), 500
+
 # Page d'accueil avec le formulaire
 @app.route('/')
 def home():
